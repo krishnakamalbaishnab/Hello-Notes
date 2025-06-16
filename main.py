4b4d7cfe34c67bab80207319413cdf33f6cf506c
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-06-17 00:12:03 +0100

    Working on the frontend part

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+conn = MongoClient("mongodb+srv://krishnakamalbaishnab:Databaseaccount%4002@cluster0.i7n38c5.mongodb.net")
+
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="index.html", context={"id": id}
-    )
+    docs = conn.hellonotes.notes.find({})
+    newDocs =[]
+    for doc in docs:
+        newDocs.append({
+            "id": doc["_id"],
+            "note" : doc["note"]
+        })
+    return templates.TemplateResponse("index.html", {"request": request, "newDocs":newDocs})
 
 
 @app.get("/")
